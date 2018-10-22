10f80d94b9087d8c2140e6e8d30a4470df4f54c3
WIN_NT\x16g020 <x16g020@chiba-fjb.ac.jp>
2018-10-22 10:23:08 +0900

    初回コミット

---
+++ app/src/main/java/x16g020/chiba_fjb/ac/jp/artest/MainActivity.java
@@ -0,0 +1,13 @@
+package x16g020.chiba_fjb.ac.jp.artest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
